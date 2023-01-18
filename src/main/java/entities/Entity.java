682590249190f5b3eb1686b0050cb479d5c25a02
Owais Dashti <owaisdashti@outlook.com>
2023-01-19 00:17:26 +0100

    deploy

--- src/main/java/entities/Entity.java
+++ src/main/java/entities/Entity.java
@@ -2,8 +2,8 @@ package entities;
 
 
 public interface Entity {
-    //Interface bruges for at vi kan refaktorere vores testkode i bl.a persist, som kan tage en Entity som
-    //parameter fremfor, at der skulle laves en persist for hvert enkelt Entity.
-    //getId() er eneste fælles metode de deler.
+    // Interface bruges for at vi kan refaktorere vores testkode i bl.a persist, som kan tage en Entity som
+    // parameter fremfor, at der skulle laves en persist for hvert enkelt Entity.
+    // getId() er eneste fælles metode de deler.
     Integer getId();
 }
